2b4a8907aebbfc7055cbaa2a07107c9ee5c3f22f
Nick Seeber <nick@techtonicgroup.com>
2019-01-01 19:07:48 -0700

    Initial Commit

---
+++ EagleBusinessBrokers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EagleBusinessBrokers.Startup))]
+namespace EagleBusinessBrokers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
